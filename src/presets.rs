dac8e95e8dd9d85172e32450e31c59521e1507cc
kmltml <kamil.tomala@gmail.com>
2019-06-02 16:57:39 +0200

    Add L1 preset

--- src/presets.rs
+++ src/presets.rs
@@ -24,6 +24,7 @@ impl Preset {
             sun_earth_moon(),
             three_stars(),
             figure_eight(),
+            lagrange_1(),
             lagrange_4()
         )
     }
@@ -178,11 +179,56 @@ fn figure_eight() -> Preset {
     }
 }
 
+fn lagrange_1() -> Preset {
+    let R: f64 = 40.0;
+    let m2: f64 = 10.0;
+    let m1 = 10000.0;
+    let r = 2.7076404184011786;
+    let v = (m1 / R).sqrt();
+    Preset {
+        name: "L1",
+        bodies: [
+            BodyData {
+                name: "Sol",
+                texture: "sun",
+                color: [5.0, 5.0, 5.0],
+                trail_color: Point3::new(0.92, 0.80, 0.49),
+                radius: 2.0,
+                mass: m1,
+                x: Vector3::new(0.0, 0.0, 0.0),
+                v: Vector3::new(0.0, 0.0, 0.0)
+            },
+            BodyData {
+                name: "Earth",
+                texture: "earth",
+                color: [1.0, 1.0, 1.0],
+                trail_color: Point3::new(0.49, 0.72, 0.92),
+                radius: 0.5,
+                mass: m2,
+                x: Vector3::new(R, 0.0, 0.0),
+                v: Vector3::new(0.0, 0.0, v)
+            },
+            BodyData {
+                name: "L1",
+                texture: "moon",
+                color: [1.0, 1.0, 1.0],
+                trail_color: Point3::new(0.94, 0.94, 0.94),
+                radius: 0.25,
+                mass: 0.1,
+                x: Vector3::new(R - r, 0.0, 0.0),
+                v: Vector3::new(0.0, 0.0, v * (R - r) / R)
+            }
+        ]
+    }
+}
+
 fn lagrange_4() -> Preset {
     let r: f64 = 20.0;
     let m1 = 1000.0;
     let m2 = 10.0;
     let v = (m1 / r).sqrt();
+    let x = (m1 - m2) / (m1 + m2) / 2.0;
+    let y = (3.0f64).sqrt() / 2.0;
     Preset {
         name: "L4",
         bodies: [
@@ -213,8 +259,8 @@ fn lagrange_4() -> Preset {
                 trail_color: Point3::new(0.94, 0.94, 0.94),
                 radius: 0.25,
                 mass: 0.1,
-                x: Vector3::new(r / 2.0, 0.0, r * 3.0f64.sqrt() / 2.0),
-                v: Vector3::new(-v * 3.0f64.sqrt() / 2.0, 0.0, v / 2.0)
+                x: Vector3::new(r * x, 0.0, r * y),
+                v: Vector3::new(-v * y, 0.0, v * x)
             }
         ]
     }
